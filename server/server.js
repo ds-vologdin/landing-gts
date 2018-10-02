4a132dfe3162e1ebb93fa854819b615055705477
Dmitry Vologdin <ds.vologdin@yandex.ru>
2018-10-02 15:40:28 +0300

    cors in express

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const express = require('express');
+var cors = require('cors');
 const account = require('./setting_mail_private.js');
-
-const app = express();
 
 const nodemailer = require('nodemailer');
 
-let transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
     host: account.host,
     port: account.port,
     auth: {
@@ -21,7 +20,20 @@ let mailOptions = {
     text: 'Hello world?',
 };
 
+const app = express();
 app.use(express.json());
+
+const whitelist = [
+    'http://192.168.128.43:8080',
+];
+const corsOptions = {
+    origin: function(origin, callback){
+        const originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+        callback(null, originIsWhitelisted);
+    },
+    credentials: true
+};
+app.use(cors(corsOptions));
 
 app.post('/landing/message/', function(request, response){
   console.log(request.body);
@@ -29,10 +41,12 @@ app.post('/landing/message/', function(request, response){
   mailOptions.subject += request.body.landing;
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
+        response.status(500);
         response.send('FAIL');
         console.log(error);
     }
   });
+  response.status(200);
   response.send('OK');
 });
 
